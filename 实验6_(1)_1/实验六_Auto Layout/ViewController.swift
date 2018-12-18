8b67c3c7701ce094e20e6888bfefc98cc6f6e864
QiuYongShan <34417758+Qiuyongshan@users.noreply.github.com>
2018-12-18 10:55:11 +0800

    Add files via upload

---
+++ 实验6_(1)_1/实验六_Auto Layout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  实验六_Auto Layout
+//
+//  Created by student on 2018/11/11.
+//  Copyright © 2018年 QYS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
